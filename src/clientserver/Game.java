5dcdb9fc4ac06e7db1ebd1f93cd57b39d073c1d6
meteoorkip <svenkonings@live.nl>
2015-01-16 16:38:14 +0100

    Game stub
    doet nog niks

---
+++ src/clientserver/Game.java
@@ -0,0 +1,37 @@
+package clientserver;
+
+import game.Board;
+
+import java.util.HashSet;
+
+public class Game {
+	private ClientHandler[] team0;
+	private ClientHandler[] team1;
+	private int currTeam;
+	private HashSet<ClientHandler> clients;
+	private Board board;
+
+	public Game(ClientHandler client, int boardX, int boardY, String[] players) {
+
+	}
+
+	public void startGame() {
+
+	}
+
+	private void requestMove() {
+
+	}
+
+	public boolean isValidMove(int col) {
+
+	}
+
+	public void doMove(int col) {
+
+	}
+
+	public String[] getPlayers() {
+
+	}
+}
