b6cb19164dc6ce4b685b81f7e43a6ea2e436736f
meteoorkip <svenkonings@live.nl>
2015-01-22 18:06:33 +0100

    Meer random maar toch slimmer smartstrat

--- src/game/SmartStrategy.java
+++ src/game/SmartStrategy.java
@@ -1,14 +1,7 @@
 package game;
 
-/**
- * SmartStrategy for the Connect4 according to the protocol of the TI-2 group.<br>
- * <br>
- * Programming Project Connect4 Module 2 Softwaresystems 2014-2015 <br>
- * 
- * @author Michael Koopman s1401335 and Sven Konings s1534130
- */
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class SmartStrategy implements Strategy {
 	public final static String NAME = "Smart";
@@ -18,7 +11,7 @@ public class SmartStrategy implements Strategy {
 	}
 
 	public int determineMove(Board b, Disc d) {
-		Set<Integer> empty = new HashSet<Integer>();
+		ArrayList<Integer> empty = new ArrayList<Integer>();
 		for (int i = 0; i < b.getColumns(); i++) {
 			if (b.isEmptyField(i)) {
 				empty.add(i);
@@ -38,28 +31,7 @@ public class SmartStrategy implements Strategy {
 				return i;
 			}
 		}
-		for (Integer i : empty) {
-			Board board = b.deepCopy();
-			board.insertDisc(i, d);
-			for (Integer j : empty) {
-				board.insertDisc(j, d);
-				if (board.gameOver()) {
-					return j;
-				}
-			}
-		}
-		for (Integer i : empty) {
-			Board board = b.deepCopy();
-			board.insertDisc(i, d.other());
-			for (Integer j : empty) {
-				board.insertDisc(j, d.other());
-				if (board.gameOver()) {
-					return j;
-				}
-			}
-		}
-		return (int) empty.toArray()[((int) (Math.random() * empty.size()))];
+		Random rng = new Random();
+		return empty.get(rng.nextInt(empty.size()));
 	}
-
-	//TODO: Implement a canWin(Board b, Disc d) method, which is mainly copy-pasta.
 }
