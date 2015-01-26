adb2d0f9c4d924f595d0f57d561d47d092ae83b1
Gamer1120 <michaelkoopmantest@gmail.com>
2015-01-26 18:26:29 +0100

    ClientView Javadoc

--- src/clientserver/ClientView.java
+++ src/clientserver/ClientView.java
@@ -8,39 +8,127 @@ package clientserver;
  * @author Michael Koopman s1401335 and Sven Konings s1534130
  */
 public interface ClientView extends MessageUI {
+
 	/**
 	 * Asks for the name of the player.
-	 * 
-	 * @return
 	 */
 	public void askName();
 
+	/**
+	 * Adds a connect message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addConnectMessage(String message);
 
+	/**
+	 * Adds an invite message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addInviteMessage(String message);
 
+	/**
+	 * Adds a move message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addMoveMessage(String message);
 
+	/**
+	 * Adds a chat message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addChatMessage(String message);
 
+	/**
+	 * Adds the board to the UI.
+	 */
 	public void addBoard();
 
+	/**
+	 * Adds a lobby message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addLobbyMessage(String message);
 
+	/**
+	 * Adds a leaderboard line message to the UI.
+	 * 
+	 * @param rank
+	 *            The rank this player is.
+	 * @param name
+	 *            The name of this player.
+	 * @param wins
+	 *            The amount of wins this player has.
+	 * @param losses
+	 *            The amount of losses this player has.
+	 * @param gamesPlayed
+	 *            The amount of games this player has played.
+	 */
 	public void addLeaderBoardLine(String rank, String name, String wins,
 			String losses, String gamesPlayed);
 
+	/**
+	 * Adds an error message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addErrorMessage(String message);
 
+	/**
+	 * Adds a ping message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addPingMessage(String message);
 
+	/**
+	 * Adds a help message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addHelpMessage(String message);
 
+	/**
+	 * Adds a hint message to the UI.
+	 * 
+	 * @param move
+	 *            The move to add.
+	 */
 	public void addHintMessage(int move);
 
+	/**
+	 * Adds a features message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addFeaturesMessage(String message);
 
+	/**
+	 * Adds a game message to the UI.
+	 * 
+	 * @param message
+	 *            The message to add.
+	 */
 	public void addGameMessage(String message);
 
-	public void addDifficultyMessage(boolean succes);
+	/**
+	 * Adds a difficulty message to the UI.
+	 * 
+	 * @param success
+	 *            If the change of difficulty was successfull.
+	 */
+	public void addDifficultyMessage(boolean success);
 }
