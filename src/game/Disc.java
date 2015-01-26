7faaeba03ac9b641c5727e4111429d39a513cf48
Gamer1120 <michaelkoopmantest@gmail.com>
2015-01-26 21:24:41 +0100

    Disc move JML + overzicht JML/Javadoc

--- src/game/Disc.java
+++ src/game/Disc.java
@@ -12,15 +12,14 @@ public enum Disc {
 
 	EMPTY, YELLOW, RED;
 
-	/*@
-		ensures this == Disc.YELLOW ==> \result == Disc.RED;
-		ensures this == Disc.RED ==> \result == Disc.YELLOW;
-		ensures this == Disc.EMPTY ==> \result == Disc.EMPTY;
-	 */
 	/**
 	 * Returns the other disc.
 	 * 
 	 * @return the other disc is this disc is not EMPTY
+	 */
+	/*@	ensures this == Disc.YELLOW ==> \result == Disc.RED;
+		ensures this == Disc.RED ==> \result == Disc.YELLOW;
+		ensures this == Disc.EMPTY ==> \result == Disc.EMPTY;
 	 */
 	public Disc other() {
 		if (this == YELLOW) {
